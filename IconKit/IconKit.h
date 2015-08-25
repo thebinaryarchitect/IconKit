52aa6e8ed160caeb2ce61b6c61e17c2148df004f
thebinaryarchitect <thebinaryarchitect@gmail.com>
2015-08-25 18:48:17 -0400

    Add license

--- IconKit/IconKit.h
+++ IconKit/IconKit.h
@@ -2,8 +2,8 @@
 //  IconKit.h
 //  IconKit
 //
-//  Created by Xiao Yao on 8/25/15.
-//  Copyright (c) 2015 Xiao Yao. All rights reserved.
+//  Copyright (C) 2015 Xiao Yao. All Rights Reserved.
+//  See LICENSE.txt for more information.
 //
 
 #import <UIKit/UIKit.h>
