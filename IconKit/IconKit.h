f23328f7930a37da9887bc746599cfaec2118819
thebinaryarchitect <thebinaryarchitect@gmail.com>
2015-08-25 18:41:36 -0400

    Initial Commit

---
+++ IconKit/IconKit.h
@@ -0,0 +1,19 @@
+//
+//  IconKit.h
+//  IconKit
+//
+//  Created by Xiao Yao on 8/25/15.
+//  Copyright (c) 2015 Xiao Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IconKit.
+FOUNDATION_EXPORT double IconKitVersionNumber;
+
+//! Project version string for IconKit.
+FOUNDATION_EXPORT const unsigned char IconKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IconKit/PublicHeader.h>
+
+
